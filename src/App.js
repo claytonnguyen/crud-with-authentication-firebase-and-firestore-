d239cec5c488e56803a7b8d0c3c3d3a3f448277c
clayton <claytonnguyen3@gmail.com>
2022-02-26 19:14:21 -0800

    added CRUD operations to the web app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 import { db } from './firebase-config';
-import Doc from './Doc.js';
 import React, { useState, useEffect } from 'react';
-import {collection, getDocs} from 'firebase/firestore';
+import {collection, getDocs, deleteDoc, addDoc, updateDoc, doc } from 'firebase/firestore';
 // Import the functions you need from the SDKs you need
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -14,12 +13,33 @@ import {collection, getDocs} from 'firebase/firestore';
 // Initialize Firebase
 
 
-
-
 function App() {
   const [entries, setEntries] = useState([]);
   const entriesCollectionRef = collection(db, "entries")
+  const [entry, setEntry] = useState("");
+
+  // Post Request
+  const postEntry = async () => {
+    await addDoc(entriesCollectionRef, {body: entry});
+    setEntry("");
+  }
+
+  // Put Request
+  const putEntry = async (id, newBody) => {
+    const newField ={ body: newBody }
+    const entryDoc = doc(db, "entries", id)
+    await updateDoc(entryDoc, newField);
+  }
+
+  // Delete Request
+  const deleteEntry = async (id) => {
+    const entryDoc = doc(db, "entries", id)
+    await deleteDoc(entryDoc);
+  }
+
   useEffect(() => {
+
+    // Get Request
     const getEntries = async () => {
       const data = await getDocs(entriesCollectionRef);
       setEntries(data.docs.map((doc) => ({...doc.data(), id: doc.id })))
@@ -31,14 +51,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <formControl>
-          <textarea placeholder='yoo'></textarea>
-        </formControl>
+        <textarea placeholder='what is your data' onChange={(e) => setEntry(e.target.value)}></textarea>
+        <button onClick={postEntry}>Post Data</button>
         <h1>Journaling Time</h1>
         {entries.map((entry) => {
           return (
             <div>
-              <h1>Entry: {entry.body}</h1>
+                <h1>Entry: {entry.body}</h1>
+                <textarea placeholder={entry.body}></textarea>
+                <button onClick={(e) => {putEntry(entry.id, entry.body)}}>Update</button>
+                <button onClick={() => {deleteEntry(entry.id)}}>Delete</button>
             </div>
           );
         })}
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -10,6 +10,18 @@ const firebaseConfig = {
     appId: "1:216716225231:web:2688630603cf1fe3e1e5ba",
     measurementId: "G-RHE0FF6T2S"
 };
+
+// const firebaseConfig = {
+//     apiKey: process.env.API_KEY,
+//     authDomain: process.env.AUTH_DOMAIN,
+//     projectId: process.env.PROJECT_ID,
+//     storageBucket: process.env.STORAGE_BUCKET,
+//     messagingSenderId: process.env.MESSAGING_SENDER_ID,
+//     appId: process.env.APP_ID,
+//     measurementId: process.env.MEASUREMENT_ID
+// };
+
+
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
