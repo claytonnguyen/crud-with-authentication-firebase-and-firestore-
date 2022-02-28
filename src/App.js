41cc81896492437050a151caef829b7e84ef6251
clayton <claytonnguyen3@gmail.com>
2022-02-28 11:24:55 -0800

    added auth with google, may need to fix database get method (reads too often)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import { db } from './firebase-config';
+import { db, signInWithGoogle, signOutWithGoogle } from './firebase-config';
 import React, { useState, useEffect } from 'react';
-import {collection, getDocs, deleteDoc, addDoc, updateDoc, doc } from 'firebase/firestore';
+import {collection, getDocs, deleteDoc, addDoc, updateDoc, doc, query, where, onSnapshot } from 'firebase/firestore';
+
 // Import the functions you need from the SDKs you need
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -13,10 +14,37 @@ import {collection, getDocs, deleteDoc, addDoc, updateDoc, doc } from 'firebase/
 // Initialize Firebase
 
 
+
 function App() {
   const [entries, setEntries] = useState([]);
   const entriesCollectionRef = collection(db, "entries")
   const [entry, setEntry] = useState("");
+  const [user, setUser] = useState(null);
+
+
+  const signIn = () => {
+    signInWithGoogle().then((result) => {
+      const name = result.user.displayName;
+      const email = result.user.email;
+      console.log(name);
+      console.log(email);
+      setUser(result.user)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  const signOut = () => {
+    signOutWithGoogle().then(() =>{
+      console.log("signedOut");
+      setUser(null);
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+  
 
   // Post Request
   const postEntry = async () => {
@@ -38,15 +66,27 @@ function App() {
   }
 
   useEffect(() => {
+    // const entriesCollectionRef = collection(db, "entries")
+    // // Get Request
+    // const getEntries = async () => {
+    //   const data = await getDocs(entriesCollectionRef);
+    //   setEntries(data.docs.map((doc) => ({...doc.data(), id: doc.id })))
+    // }
+    // getEntries();
+    const q = query(collection(db, "entries"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      setEntries(querySnapshot.docs.map((doc) => ({...doc.data(), id: doc.id })))
+    }, [entries])})
 
-    // Get Request
-    const getEntries = async () => {
-      const data = await getDocs(entriesCollectionRef);
-      setEntries(data.docs.map((doc) => ({...doc.data(), id: doc.id })))
-    }
-
-    getEntries();
-  }, [entries])
+  // useEffect(() => {
+  //   const auth = getAuth();
+  //   setUser(auth.currentUser);
+  //   if(user){
+  //     console.log(user.displayName)
+  //   } else {
+  //     console.log("No user")
+  //   }
+  // }, [user])
 
   return (
     <div className="App">
@@ -54,6 +94,8 @@ function App() {
         <textarea placeholder='what is your data' onChange={(e) => setEntry(e.target.value)}></textarea>
         <button onClick={postEntry}>Post Data</button>
         <h1>Journaling Time</h1>
+        {user ? <button onClick={signOut}>Sign Out</button> : <button id="SignIn" onClick={signIn}>Sign In</button>}
+        {user ? <h1>You signed in Kizzy, {user.displayName}</h1> : <h1>Sign In Kizzy</h1>}
         {entries.map((entry) => {
           return (
             <div>
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,15 +1,36 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAcGKdFl3Y7aVJlgZL96x085tBQIBvA5N8",
-    authDomain: "journal-6c65a.firebaseapp.com",
-    projectId: "journal-6c65a",
-    storageBucket: "journal-6c65a.appspot.com",
-    messagingSenderId: "216716225231",
-    appId: "1:216716225231:web:2688630603cf1fe3e1e5ba",
-    measurementId: "G-RHE0FF6T2S"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAcGKdFl3Y7aVJlgZL96x085tBQIBvA5N8",
+//     authDomain: "journal-6c65a.firebaseapp.com",
+//     projectId: "journal-6c65a",
+//     storageBucket: "journal-6c65a.appspot.com",
+//     messagingSenderId: "216716225231",
+//     appId: "1:216716225231:web:2688630603cf1fe3e1e5ba",
+//     measurementId: "G-RHE0FF6T2S"
+// };
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCm86LYGx9gJnuDEGYh1KpROwn2OUIiWeQ",
+//     authDomain: "fake-b22d5.firebaseapp.com",
+//     projectId: "fake-b22d5",
+//     storageBucket: "fake-b22d5.appspot.com",
+//     messagingSenderId: "1099253475977",
+//     appId: "1:1099253475977:web:ef133ac8f65099d3eb7458",
+//     measurementId: "G-DLHTGJSZQG"
+//   };
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBMxQ4-5lYVlGYGvdjTcU0i4nR_XLaVzho",
+    authDomain: "new-fake.firebaseapp.com",
+    projectId: "new-fake",
+    storageBucket: "new-fake.appspot.com",
+    messagingSenderId: "1025779881350",
+    appId: "1:1025779881350:web:9abd91b5a627c388b64807",
+    measurementId: "G-DBLDKXTLR8"
+  };
 
 // const firebaseConfig = {
 //     apiKey: process.env.API_KEY,
@@ -25,3 +46,11 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+
+export const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export const signInWithGoogle =  () => signInWithPopup(auth, provider);
+
+
+export const signOutWithGoogle = async () => signOut(auth);
